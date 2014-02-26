1894ca99dc3ba7f36c8af4dc8daaa28b527928ee
Pavel Poliakov <pavlop@Pavels-MacBook-Pro.local>
2014-02-25 23:10:41 -0800

    Remove duplicates form a string

---
+++ src/interview/datastructures/RemoveDuplicatesNoUseDataStructures.java
@@ -0,0 +1,63 @@
+package interview.datastructures;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by pavlop on 2/25/14.
+ */
+public class RemoveDuplicatesNoUseDataStructures {
+    public static String perform(String str) {
+
+        // this is a brute force solution
+        // Optimization: 
+        // if the order can be changed
+        // you can always move chars from the tail
+
+        char[] chars = str.toCharArray();
+        int newLength = chars.length;
+
+        for(int i = 0; i < newLength; i++) {
+            for(int j = i+1; j <newLength;){
+                if (chars[i] == chars[j]) {
+                    //shift the array
+                    for (int k = j; k < newLength -1; k++) {
+                        chars[k] = chars[k+1];
+                    }
+                    newLength--;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return new String(Arrays.copyOf(chars, newLength));
+    }
+
+    @Test
+    public void positive() {
+        assertEquals("a",perform("aaa"));
+        assertEquals(perform("aabbcc"), "abc");
+        assertEquals(perform("aa"), "a");
+        assertEquals(perform("aa"), "a");
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    @Test
+    public void negative(){
+        assertEquals("",perform(""));
+        assertEquals("",perform(""));
+        exception.expect(Exception.class);
+        perform(null);
+    }
+
+
+
+}
